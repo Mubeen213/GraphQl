e972c1618383b49bba9acc0b4fe43f1f607648ce
Mubeen <mdmubeen1307@gmail.com>
2022-05-07 23:24:20 +0530

    Movie schema

---
+++ schema/MockMovies.js
@@ -0,0 +1,28 @@
+const MoviesList = [
+  {
+    id: 1,
+    name: 'Aladdin',
+    yearOfPublication: 2019,
+    isInTheatres: false,
+  },
+  {
+    id: 1,
+    name: 'Fast And furious',
+    yearOfPublication: 2022,
+    isInTheatres: true,
+  },
+  {
+    id: 1,
+    name: 'Money Heist',
+    yearOfPublication: 2021,
+    isInTheatres: false,
+  },
+  {
+    id: 1,
+    name: 'The Witcher',
+    yearOfPublication: 2021,
+    isInTheatres: true,
+  },
+];
+
+module.exports = { MoviesList };
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,26 @@
-const { UserList } = require('./Mockdata');
-
+const { UserList } = require('./MockData');
+const { MoviesList } = require('./MockMovies');
+const _ = require('lodash');
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+
+    //Movie Resolvers
+    movies: () => {
+      return MoviesList;
+    },
+
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MoviesList, { name });
+      return movie;
     },
   },
 };
